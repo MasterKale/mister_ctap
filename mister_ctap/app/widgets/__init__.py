6d19e0b1bb97853917a6c134514420df57c72355
Matthew Miller <matthew@millerti.me>
2021-11-04 22:38:58 -0700

    Add a custom header

--- mister_ctap/app/widgets/__init__.py
+++ mister_ctap/app/widgets/__init__.py
@@ -1,1 +1,2 @@
 from .authenticators_list import AuthenticatorsList
+from .header import MisterCtapHeader
---
+++ mister_ctap/app/widgets/header.py
@@ -0,0 +1,29 @@
+from rich.console import RenderableType
+from rich.panel import Panel
+from rich.style import Style
+from rich.table import Table
+from textual import events
+from textual.reactive import Reactive
+from textual.widgets import Header
+
+
+class MisterCtapHeader(Header):
+    """Override the default Header for Styling"""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.tall = True
+
+    def render(self) -> Table:
+        header_table = Table.grid(padding=(0, 1), expand=True)
+        header_table.add_column(justify="left", ratio=0, width=8)
+        header_table.add_column("title", justify="center", ratio=1)
+        header_table.add_column("clock", justify="right", width=8)
+        header_table.add_row(
+            "🔐", self.full_title, self.get_clock() if self.clock else ""
+        )
+        header: RenderableType
+        header = (
+            Panel(header_table, style=header_table.style) if self.tall else header_table
+        )
+        return header
